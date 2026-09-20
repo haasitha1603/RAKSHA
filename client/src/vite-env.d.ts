a62ac136910cebb5b7245cc8550c2b1b6e6abe10
Haasitha <haasitha03@gmail.com>
2026-09-20 17:05:49 +0530

    fix(client): ensure all build deps in dependencies, use vite build for production bundle, include dev deps on Render build

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+declare module '*.css';
+
+declare module 'workbox-precaching';
+declare module 'workbox-routing';
+declare module 'workbox-strategies';
+
+interface ServiceWorkerGlobalScope {
+  __WB_MANIFEST: any;
+}
